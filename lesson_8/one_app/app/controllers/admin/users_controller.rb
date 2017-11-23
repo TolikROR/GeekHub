a8164290b14651acf54fe1c101772ae9f14747e9
Tolik <anatoliyboyko1995@gmail.com>
2017-11-23 19:36:58 +0200

    add lesson_8 with one_app

---
+++ lesson_8/one_app/app/controllers/admin/users_controller.rb
@@ -0,0 +1,14 @@
+class Admin::UsersController < AdminsController
+  def index
+    @users = User.all
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+
+    @user.destroy
+
+    redirect_to admin_users_path
+  end
+
+end
---
+++ lesson_8/one_app/app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_admin
+
+
+  def current_admin
+    @current_user.admin
+  end
+end
