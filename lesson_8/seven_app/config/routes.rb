142a20e6725ec93928cfa55ea45b68044fe54686
Tolik <anatoliyboyko1995@gmail.com>
2017-11-29 18:52:32 +0200

    add seven app

---
+++ lesson_8/seven_app/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root 'welcome#index'
+
+  namespace :admin do
+    resources :users, only: [:index, :destroy]
+    root 'users#index'
+  end
+
+
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
