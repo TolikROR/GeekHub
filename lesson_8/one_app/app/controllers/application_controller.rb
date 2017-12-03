693c3d98fe9e4ca40ac58247f04f4eec827b7588
Tolik <anatoliyboyko1995@gmail.com>
2017-12-03 20:29:44 +0200

    add avatar in devise:users

--- lesson_8/one_app/app/controllers/application_controller.rb
+++ lesson_8/one_app/app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_admin
 
+  private
+
   def current_admin
-    @current_user.admin
+    @current_user.admin if current_user
   end
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:email, :password, :password_confirmation, :avatar]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
+
 end
--- lesson_8/three_app/app/controllers/application_controller.rb
+++ lesson_8/three_app/app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   helper_method :current_admin
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def current_admin
     current_user.admin
   end
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:email, :password, :password_confirmation, :avatar]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
+
 end
